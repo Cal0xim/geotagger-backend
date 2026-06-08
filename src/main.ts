8814e635989a80d384ab0358969e2d47479ee00e
Cal0xim <blazhos@gmail.comgit config --global user.email blazhos@gmail.comgit config --global user.email blazhos@gmail.com>
2026-06-08 15:09:46 +0200

    feat: backend project setup

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('GeoTagger API')
+  .setDescription('GeoTagger REST API')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
